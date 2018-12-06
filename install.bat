42f914ed70dbbf168faa3400598e223aeb4b871e
Eike Siewertsen <eike@technocreatives.com>
2018-12-06 16:23:19 +0100

    remove installer files as this is handled by divvun-bundler now

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist "C:\Program Files\SpellCheckTest\" mkdir "C:\Program Files\SpellCheckTest\"
-if not exist "C:\Program Files\SpellCheckTest\dicts" mkdir "C:\Program Files\SpellCheckTest\dicts"
-copy target\debug\winspellcheck.dll "C:\Program Files\SpellCheckTest\"
-regedit.exe /s localmachine.reg
